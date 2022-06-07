490170eea851d3540cd1b061c165b94c37a15988
vaishnav-mk <84540554+vaishnav-mk@users.noreply.github.com>
2022-06-07 18:45:10 +0300

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -10,16 +10,15 @@ import {
 import Time from '@/components/Time.jsx'
 import Weather from '@/components/Weather.jsx'
 import Link from 'next/link';
-import NowPlaying from '@/components/NowPlaying.jsx'
 export default function Hero() {
 return (
 <div className="mb-20">
    <h1 className="mt-5 mb-4">{hero.title}</h1>
    <p className="text-lg"><FontAwesomeIcon icon={faCloud}/> {hero.desc}</p>
   <p className="text-lg"><FontAwesomeIcon icon={faClock}/> {Time()}</p>
-  <p><FontAwesomeIcon icon={faCloud}/> It's currently [weather] in xxxxx</p>
-  <p><FontAwesomeIcon icon={faCloud}/> Currently listening to: []!</p>
-  <p><FontAwesomeIcon icon={faCalendarDay}/> 69 Years old</p>
+  <p><FontAwesomeIcon icon={faCloud}/> It's currently 38C in Doha</p>
+  <p><FontAwesomeIcon icon={faCloud}/> Currently listening to: Nothing!</p>
+  <p><FontAwesomeIcon icon={faCalendarDay}/> 18 Years old</p>
 </div>
 );
 }
